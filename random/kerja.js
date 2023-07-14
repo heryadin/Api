c7456163db6a3f14680eaa3a0838ed1e121713f2
hariyadin <79754073+heryadin@users.noreply.github.com>
2023-07-14 23:21:11 +0700

    Create kerja.js

---
+++ random/kerja.js
@@ -0,0 +1,146 @@
+[
+
+{
+    "indeks": 0,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sebagai pemotong kue dan Anda menang"
+  },
+  {
+    "indeks": 1,
+    "creator": "haryadin",
+    "kerja": "Bekerja di perusahaan militer swasta, berpenghasilan"
+  },
+  {
+    "indeks": 2,
+    "creator": "haryadin",
+    "kerja": "Atur acara mencicipi anggur dan dapatkan"
+  },
+  {
+  "indeks": 3,
+    "creator": "haryadin",
+    "kerja": "Anda diculik dan dibawa ke koloseum bawah tanah tempat Anda melawan monster dengan orang yang belum pernah Anda temui sebelumnya. Anda menang."
+  },
+  {
+  "indeks": 4,
+    "creator": "haryadin",
+    "kerja": "Bersihkan cerobong asap dan temukan"
+  },
+  {
+  "indeks": 5,
+    "creator": "haryadin",
+    "kerja": "Anda mengembangkan game untuk mencari nafkah dan Anda menang"
+  },
+  {
+  "indeks": 6,
+    "creator": "haryadin",
+    "kerja": "Mengapa perintah ini disebut bekerja? Anda bahkan tidak melakukan apa pun yang berhubungan dengan pekerjaan. Namun Anda menang"
+  },
+  {
+  "indeks": 7,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja lembur di kantor untuk"
+  },
+  {
+  "indeks": 8,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sebagai penculik pengantin dan menang"
+  },
+  {
+  "indeks": 9,
+    "creator": "haryadin",
+    "kerja": "Seseorang datang dan bermain. Anda terpukul karena menonton"
+  },
+ {
+ "indeks": 10,
+    "creator": "haryadin",
+    "kerja": "Anda membeli dan menjual item dan Memperoleh"
+  },
+{
+  "indeks": 11,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja di restoran Nenek sebagai juru masak dan menang"
+  },
+  {
+  "indeks": 12,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja 10 menit di Pizza Hut setempat. Anda menang"
+  },
+  {
+  "indeks": 13,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sebagai creator kue keberuntungan dan Anda menang"
+  },
+  {
+  "indeks": 14,
+    "creator": "haryadin",
+    "kerja": "Anda memeriksa tas Anda dan memutuskan untuk menjual beberapa barang tidak berguna yang tidak Anda perlukan. Ternyata semua sampah itu sepadan"
+  },
+  {
+  "indeks": 15,
+    "creator": "haryadin",
+    "kerja": "Anda melihat seseorang berjuang untuk memasukkan sebuah kotak ke dalam mobilnya, Anda segera membantu mereka sebelum mereka terluka. Setelah membantu mereka, mereka dengan baik hati memberi Anda"
+  },
+  {
+  "indeks": 16,
+    "creator": "haryadin",
+    "kerja": "Anda mengembangkan game untuk mencari nafkah dan Anda menang"
+  },
+  {
+  "indeks": 17,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sepanjang hari di perusahaan untuk"
+  },
+  {
+  "indeks": 18,
+    "creator": "haryadin",
+    "kerja": "Anda membantu memoderasi grup DyLux dengan"
+  },
+  {
+  "indeks": 19,
+    "creator": "haryadin",
+    "kerja": "Anda mendesain logo untuk *FG* oleh"
+  },
+  {
+  "indeks": 20,
+    "creator": "haryadin",
+    "kerja": "Anda memoderasi grup saat *FG* pergi, pembayaran telah dilakukan"
+  },
+  {
+  "indeks": 21,
+    "creator": "haryadin",
+    "kerja": "Dia bekerja sebaik mungkin di perusahaan percetakan yang membuka lowongan dan mendapatkan bagian yang adil!"
+  },
+  {
+  "indeks": 22,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sebagai pemangkas semak untuk *haryadin* dan menang"
+  },
+  {
+  "indeks": 23,
+    "creator": "haryadin",
+    "kerja": "Permintaan untuk game seluler telah meningkat, jadi Anda membuat game baru yang penuh dengan transaksi mikro. Dengan game baru Anda, Anda mendapatkan total"
+  },
+  {
+  "indeks": 24,
+    "creator": "haryadin",
+    "kerja": "Anda bekerja sebagai pengisi suara Spongebob dan Anda berhasil menang"
+
+  },
+  {
+  "indeks": 39,
+    "creator": "haryadin",
+    "kerja": "Anda memperbaiki mesin arkade dan menerima"
+  },
+  {
+  "indeks": 40,
+    "creator": "haryadin",
+    "kerja": "Hanya dengan berada di grup *DyLux - FG* Anda menang!"
+  },
+  {
+  "indeks": 41,
+    "creator": "haryadin",
+    "kerja": "Anda melakukan kerja sosial untuk tujuan yang baik! Anda mendapatkannya!"
+  }
+  
+]
+  
